372f85b9e54addd7dbf08989761bc805781a4c7d
Dmitry Gusev <dmtrxiii@gmail.com>
2021-12-22 15:26:27 +0300

    tasks released

---
+++ src/main/java/com/ineedyourcode/algorithms/lesson_6/Lesson_6.java
@@ -0,0 +1,31 @@
+package com.ineedyourcode.algorithms.lesson_6;
+
+public class Lesson_6 {
+    public static void main(String[] args) {
+        int count = 20;
+        int countBalance = 0;
+        for (int i = 0; i < count; i++) {
+            MyTree<Integer, Integer> map = new MyTree<>();
+            while (true) {
+                int value = randomValue();
+                int key = value;
+                map.put(key, value);
+                if (map.height() == 4) {
+                    map.deleteToKey(key);
+                    if (map.isBalance()) {
+                        countBalance++;
+                    }
+                    break;
+                }
+            }
+        }
+        System.out.println("Сбалансированных деревьев: " + countBalance);
+        System.out.println("Общее число деревьев: " + count);
+        System.out.println("Процент сбалансированных деревьев: " + (int)((countBalance / (double) count * 100)) + " %");
+    }
+
+    public static int randomValue() {
+        return (int) ((Math.random() * 51) - 26);
+    }
+}
+
